53917b286f971eda9facc3d55c7c204b77fa9a06
Aryan Sindhi <37263346+aryansindhi18@users.noreply.github.com>
2025-02-12 10:50:29 +0000

    created Customer Class

---
+++ src/main/java/bank/Customer.java
@@ -0,0 +1,57 @@
+package bank;
+
+public class Customer {
+  private int id;
+  private String username;
+  private String name;
+  private String password;
+  private int accountId;
+
+  public Customer(int id, String username, String name, String password, int accountId) {
+    setId(id);
+    setUsername(username);
+    setName(name);
+    setPassword(password);
+    setAccountId(accountId);
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return this.username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getPassword() {
+    return this.password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public int getAccountId() {
+    return this.accountId;
+  }
+
+  public void setAccountId(int accountId) {
+    this.accountId = accountId;
+  }
+}
